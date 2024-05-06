1905015ce167bc2c40444be393bd0c39b27e7370
Emily Wahlin <emilysaletti@gmail.com>
2024-05-06 21:47:52 +0200

    wrote function

--- firebaseBlog/src/App.tsx
+++ firebaseBlog/src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import WritePost from "./components/writeBlogPost"
 
 import './App.css'
 
 function App() {
-  
+
   return (
     <>
-
+	<h1>The Everything Blog</h1>
+		<WritePost/>
     </>
   )
 }
